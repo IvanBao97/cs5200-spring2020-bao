54344aa6cf1f8c311716dec55e0c89ca027727fc
IvanBao97 <55570849+IvanBao97@users.noreply.github.com>
2020-01-27 20:30:45 -0500

    first commit

---
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2020BaoApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Spring2020BaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs5200Spring2020BaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/northeastern/cs5200/Cs5200Spring2020BaoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs5200;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs5200Spring2020BaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
